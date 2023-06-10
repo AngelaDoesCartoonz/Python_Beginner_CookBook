cb8521e6f432b803684bf814441d44ee0799fa42
aigamedevz <136177051+AngelaDoesCartoonz@users.noreply.github.com>
2023-06-10 10:15:32 -0500

    Created "HelloWorld.py" and the usage of Strings in Python.
    I learned the basics of how to use STRINGS in python and incorporated text in-between strings to get a better idea of how they work.

---
+++ PythonTutorials/HelloWorld.py
@@ -0,0 +1,12 @@
+print ('Hello World!')
+print ('In PYTHON I am called a string!')
+print ('You can use me to input text in between quotation marks.')
+
+print ('For Example:')
+print ('Moo')
+print ('The cow makes that sound')
+print ('Cluck goes the chicken')
+
+print ('Dialogue Example:')
+print ('Questrina: How do you use a string?')
+print ('Answerette: Its pretty easy all you have to include in strings are quotation marks and put text in-between them.')
